bd8b358ec9f8bd390c7a76934a634ba3751660a7
Zardoz8901 <interzoneclearinghouse@gmail.com>
2022-12-08 15:51:09 +0100

    Add js and clarify form

---
+++ script.js
@@ -0,0 +1,7 @@
+const firstName = document.getElementById('first-name')
+const lastName = document.getElementById('last-name')
+const email = document.getElementById('email')
+const emailConfirm = document.getElementById('email-confirm')
+const password = document.getElementById('password')
+const passwordConfirm = document.getElementById('password-confirm')
+
