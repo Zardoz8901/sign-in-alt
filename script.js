619b64c0e6e604ec642163d5436b19f973e93e8b
Zardoz8901 <interzoneclearinghouse@gmail.com>
2022-12-09 13:25:02 +0100

    Add email and password validation

--- script.js
+++ script.js
@@ -1,7 +1,47 @@
-const firstName = document.getElementById('first-name')
-const lastName = document.getElementById('last-name')
-const email = document.getElementById('email')
-const emailConfirm = document.getElementById('email-confirm')
-const password = document.getElementById('password')
-const passwordConfirm = document.getElementById('password-confirm')
+const firstName = document.getElementById('first-name');
+const lastName = document.getElementById('last-name');
+const email = document.getElementById('email');
+const emailConfirm = document.getElementById('email-confirm');
+const password = document.getElementById('password');
+const passwordConfirm = document.getElementById('password-confirm');
+const form = document.getElementById('form');
+const errorElement = document.getElementById('error');
+
+form.addEventListener('submit', (e) => {
+    let messages = []
+    if (firstName.value.length == 0) {
+        firstName.setAttribute('required','');
+        e.preventDefault()
+    }
+    if (lastName.value.length == 0) {
+        lastName.setAttribute('required','')
+        e.preventDefault()
+    }
+    if (email.value.length >= 0) {
+        email.setAttribute('required','')
+    }
+    if (emailConfirm.value.length >= 0) {
+        emailConfirm.setAttribute('required','')
+    }
+    if (emailConfirm.value != email.value) {
+        emailConfirm.setAttribute('required','')
+        messages.push('Match email')
+        e.preventDefault()
+        
+    }
+    if (password.value.length >= 0) {
+        password.setAttribute('required','')
+    }
+    if (passwordConfirm.value.length >= 0) {
+        passwordConfirm.setAttribute('required','')
+    }
+    if (passwordConfirm.value != password.value) {
+        passwordConfirm.setAttribute('required','')
+        messages.push('Match password')
+        e.preventDefault()    
+    }
+    if (messages.length > 0)
+    e.preventDefault()
+    errorElement.innerText = messages.join('. ')
+});
 
